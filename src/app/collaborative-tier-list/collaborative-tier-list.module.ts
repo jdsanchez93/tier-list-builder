34f41f7feb73a593391cd855704e01d7147448d8
jd <jdsanchez2011@gmail.com>
2022-02-14 22:32:06 -0700

    feat: add module

---
+++ src/app/collaborative-tier-list/collaborative-tier-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollaborativeTierListRoutingModule } from './collaborative-tier-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CollaborativeTierListRoutingModule
+  ]
+})
+export class CollaborativeTierListModule { }
