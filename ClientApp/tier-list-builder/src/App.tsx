7c492235e2427ee591c8ab4c09710609eed8c52a
jd <jdsanchez2011@gmail.com>
2022-11-13 01:19:33 -0700

    feat: routing & page layout

--- ClientApp/tier-list-builder/src/App.tsx
+++ ClientApp/tier-list-builder/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import ResponsiveDrawer from './app/ResponsiveDrawer';
+import { Route, Routes } from 'react-router-dom';
 
 const darkTheme = createTheme({
   palette: {
@@ -16,51 +17,11 @@ function App() {
     <div className="App">
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Counter />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <span>
-            <span>Learn </span>
-            <a
-              className="App-link"
-              href="https://reactjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Redux
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux-toolkit.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Redux Toolkit
-            </a>
-            ,<span> and </span>
-            <a
-              className="App-link"
-              href="https://react-redux.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React Redux
-            </a>
-          </span>
-        </header>
+        <Routes>
+          <Route path="/" element={<ResponsiveDrawer />}>
+            <Route path="/all" element={<Counter />}></Route>
+          </Route>
+        </Routes>
       </ThemeProvider>
     </div>
   );
