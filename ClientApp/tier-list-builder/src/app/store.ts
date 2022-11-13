f73a7e5c1a7ea3e53f837a45f12c742ff19abf00
jd <jdsanchez2011@gmail.com>
2022-11-13 01:47:13 -0700

    create tier list slice & add to store

--- ClientApp/tier-list-builder/src/app/store.ts
+++ ClientApp/tier-list-builder/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import tierListReducer from '../features/tier-list/tierListSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    tierList: tierListReducer
   },
 });
 
