cbb6637681fbeaa3b461b61fbdc2f069ee63bfd9
jd <jdsanchez2011@gmail.com>
2023-01-18 00:20:32 -0700

    restructure configure tier list to consume prop

--- ClientApp/tier-list-builder/src/features/configure-tier-list/ConfigureTierList.tsx
+++ ClientApp/tier-list-builder/src/features/configure-tier-list/ConfigureTierList.tsx
@@ -1,19 +1,18 @@
-import { Box, CircularProgress, Typography } from '@mui/material';
-import React, { useEffect, useMemo, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useGetTierListByIdQuery } from '../api/apiSlice';
+import React, { useMemo, useState } from 'react';
+import { Box, Typography } from '@mui/material';
 import { DraggableTierListRows } from './DraggableTierListRows';
 import { AddRowForm } from './AddRowForm';
 import { ConfigureTierListForm } from './ConfigureTierListForm';
 import { SaveTierList } from './SaveTierList';
 import { TierList, TierListRow } from '../tier-list/TierList.models';
 
-export function ConfigureTierList() {
+interface ConfigureTierListProps {
+    tierList: TierList;
+}
 
-    const { tierListId } = useParams();
-    const { data: tierList, isSuccess, isLoading } = useGetTierListByIdQuery(tierListId);
+export function ConfigureTierList(props: ConfigureTierListProps) {
 
-    const [tierListState, setTierListState] = useState<TierList>(tierList || { tierListId: 0, name: '', tierListRows: [] });
+    const [tierListState, setTierListState] = useState<TierList>(props.tierList);
 
     const handleNameChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> =
         (e: React.ChangeEvent<HTMLInputElement>) => setTierListState(t => ({ ...t, name: e.target.value }))
@@ -21,43 +20,43 @@ export function ConfigureTierList() {
     const handleRowsChange =
         (r: TierListRow[]) => setTierListState(t => ({ ...t, tierListRows: r }));
 
-    useEffect(() => {
-        if (tierList !== undefined) {
-            setTierListState(tierList);
-        }
-    }, [tierList])
+    const configName = useMemo(
+        () => (
+            <ConfigureTierListForm
+                onChange={handleNameChange}
+                tierListName={tierListState.name}
+            />
+        ),
+        [tierListState.name]
+    );
 
-    const tierListForm = useMemo(() => <ConfigureTierListForm onChange={handleNameChange} tierListName={tierListState.name} />, [tierListState.name]);
-    const draggableRows = useMemo(() => <DraggableTierListRows onChange={handleRowsChange} rows={tierListState.tierListRows || []} />, [tierListState.tierListRows]);
-    const addRowForm = useMemo(() => <AddRowForm onChange={handleRowsChange} tierListId={tierListState.tierListId} rows={tierListState.tierListRows || []} />, [tierListState.tierListRows, tierListState.tierListId]);
+    const draggableRows = useMemo(
+        () => (
+            <DraggableTierListRows
+                onChange={handleRowsChange}
+                rows={tierListState.tierListRows || []}
+            />
+        ),
+        [tierListState.tierListRows]
+    );
 
-    let content;
-
-    // TODO consider handling other states
-    if (isLoading) {
-        content = <CircularProgress />
-    } else if (isSuccess) {
-
-        content = (
-            <Box>
-                {tierListForm}
-                {draggableRows}
-                {addRowForm}
-                <SaveTierList tierList={tierListState} />
-            </Box>
-        );
-    }
+    const addRowForm = useMemo(
+        () => (
+            <AddRowForm
+                onChange={handleRowsChange}
+                tierListId={tierListState.tierListId}
+                rows={tierListState.tierListRows || []}
+            />
+        ),
+        [tierListState.tierListRows, tierListState.tierListId]
+    );
 
     return (
-        <div>
-            <Typography variant="h6">
-                Configure Tier List
-            </Typography>
-
-            <br />
-
-            {content}
-
-        </div>
+        <Box>
+            {configName}
+            {draggableRows}
+            {addRowForm}
+            <SaveTierList tierList={tierListState} />
+        </Box>
     )
 }
--- ClientApp/tier-list-builder/src/features/configure-tier-list/EditTierList.tsx
+++ ClientApp/tier-list-builder/src/features/configure-tier-list/EditTierList.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { CircularProgress, Typography } from '@mui/material';
+import { useGetTierListByIdQuery } from '../api/apiSlice';
 import { ConfigureTierList } from './ConfigureTierList';
 
 export default function EditTierList() {
+
+    const { tierListId } = useParams();
+    const { data: tierList, isSuccess, isLoading } = useGetTierListByIdQuery(tierListId);
+
+    let content;
+
+    // TODO consider handling other states
+    if (isLoading) {
+        content = <CircularProgress />
+    } else if (isSuccess) {
+        content = (
+            <ConfigureTierList tierList={tierList} />
+        );
+    }
+
     return (
-        <ConfigureTierList></ConfigureTierList>
+        <div>
+            <Typography variant="h6">
+                Edit Tier List
+            </Typography>
+
+            <br />
+
+            {content}
+
+        </div>
     )
 }
