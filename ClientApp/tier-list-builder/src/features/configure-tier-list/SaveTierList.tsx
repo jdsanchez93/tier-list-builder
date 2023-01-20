faac4fbd2c05806a945ffaf681f02c04fd3829e2
jd <jdsanchez2011@gmail.com>
2023-01-20 12:54:56 -0700

    fix: post tier list on create; don't save tier list with blank name

--- ClientApp/tier-list-builder/src/features/configure-tier-list/SaveTierList.tsx
+++ ClientApp/tier-list-builder/src/features/configure-tier-list/SaveTierList.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Fab } from '@mui/material';
 import React from 'react';
-import { useEditTierListMutation } from '../api/apiSlice';
+import { useEditTierListMutation, usePostTierListMutation } from '../api/apiSlice';
 import { TierList } from '../tier-list/TierList.models';
 import SaveIcon from '@mui/icons-material/Save';
 
@@ -10,24 +10,34 @@ interface SaveTierListProps {
 
 export const SaveTierList = (props: SaveTierListProps) => {
     const [patchTierList] = useEditTierListMutation();
+    const [createTierList] = usePostTierListMutation();
 
     const onSubmit = async () => {
         try {
-            if (props.tierList.tierListId === 0) {
-                // TODO post new tier list
-                console.log('create new tier list...')
+            if (props.tierList.name === '') {
+                console.error('Name cannot be blank');
                 return;
             }
+
             let partialTierList: Partial<TierList> = props.tierList;
+            if (props.tierList.tierListId === 0) {
+                createTierList(partialTierList);
+                return;
+            }
             patchTierList(partialTierList);
         } catch (error) {
-            console.log(error);
+            console.error(error);
         }
     }
 
     return (
         <Box
-            sx={{ margin: '10px', display: 'flex', justifyContent: 'right', gap: '20px' }}
+            sx={{
+                margin: '10px',
+                display: 'flex',
+                justifyContent: 'right',
+                gap: '20px'
+            }}
         >
             <Fab variant="extended" onClick={onSubmit}>
                 <SaveIcon sx={{ mr: 1 }} />
