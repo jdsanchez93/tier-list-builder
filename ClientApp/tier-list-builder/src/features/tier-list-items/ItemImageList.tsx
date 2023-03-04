2248479e138e810df2d5cd8f9f416805344fa3ec
jd <jdsanchez2011@gmail.com>
2023-03-04 01:51:40 -0700

    feat: image list item bar action to delete item

--- ClientApp/tier-list-builder/src/features/tier-list-items/ItemImageList.tsx
+++ ClientApp/tier-list-builder/src/features/tier-list-items/ItemImageList.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Box, CircularProgress, ImageList, ImageListItem } from '@mui/material';
-import { useGetItemsByTierListIdQuery } from '../api/apiSlice';
+import { Avatar, Box, CircularProgress, IconButton, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+import { useDeleteItemMutation, useGetItemsByTierListIdQuery } from '../api/apiSlice';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface ItemImageListProps {
     tierListId: number;
 }
 export default function ItemImageList({ tierListId }: ItemImageListProps) {
     const { data: tierListItems, isSuccess, isLoading } = useGetItemsByTierListIdQuery(tierListId);
+    const [deleteItem] = useDeleteItemMutation();
 
     let content;
     if (isLoading) {
@@ -15,13 +17,26 @@ export default function ItemImageList({ tierListId }: ItemImageListProps) {
         );
     } else if (isSuccess) {
         content = (
-            <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={100}>
+            <ImageList sx={{ width: 300 }} cols={2} rowHeight={150}>
                 {tierListItems.map((item) => (
                     <ImageListItem key={item.tierListItemId}>
                         <img
                             src={item.presignedUrl}
                             alt={item.name}
                             loading="lazy"
+                        />
+                        <ImageListItemBar
+                            title={item.name}
+                            position={'bottom'}
+                            actionIcon={
+                                <IconButton
+                                    onClick={() => deleteItem(item.tierListItemId)}
+                                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                    aria-label={`info about ${item.name}`}
+                                >
+                                    <DeleteIcon />
+                                </IconButton>
+                            }
                         />
                     </ImageListItem>
                 ))}
